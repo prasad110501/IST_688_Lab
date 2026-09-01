1470b58e9043a883085920732ef0858819396398
prasad110501 <pshimpi@syr.edu>
2026-09-01 15:40:19 +0000

    added the secret key and some changes

--- Lab2.py
+++ Lab2.py
@@ -1,5 +1,58 @@
-import streamlit as st 
+import streamlit as st
+from openai import OpenAI
 
-st.title("Lab 2")
-st.set_page_config(page_title= "Lab 1 HCAI", page_icon=None, layout="wide",
-initial_sidebar_state="auto")
+# Show title and description.
+st.title("📄 Document question answering")
+st.write(
+    "Upload a document below and ask a question about it – GPT will answer! "
+    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
+)
+
+# Ask user for their OpenAI API key via `st.text_input`.
+# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
+# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
+openai_api_key = st.secrets.OPENAI_API_KEY
+
+if not openai_api_key:
+    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+else:
+
+    # Create an OpenAI client.
+    client = OpenAI(api_key=openai_api_key)
+
+    # Let the user upload a file via `st.file_uploader`.
+    uploaded_file = st.file_uploader(
+        "Upload a document (.txt or .md)", type=("txt", "md")
+    )
+
+    # Ask the user for a question via `st.text_area`.
+    question = st.selectbox(
+        "Select from a prompt",
+        ["Summarize the document in 100 words","Summarize the document in 2 connecting paragraphs","Summarize the document in 5 bullet points"]
+    )
+    # Adding the model choice 
+    model_choice = st.selectbox(
+        "Choose a model to test",
+        ["gpt-3.5-turbo", "gpt-4.1", "gpt-5-chat-latest", "gpt-5-nano"]
+    )
+
+    if uploaded_file and question:
+
+        # Process the uploaded file and question.
+        document = uploaded_file.read().decode()
+        messages = [
+            {
+                "role": "user",
+                "content": f"Here's a document: {document} \n\n---\n\n {question}",
+            }
+        ]
+
+        # Generate an answer using the OpenAI API.
+        stream = client.chat.completions.create(
+            model=model_choice,
+            messages=messages,
+            stream=True,
+        )
+
+        # Stream the response to the app using `st.write_stream`.
+        st.write_stream(stream)
