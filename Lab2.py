69bb2098ec693c9b742aa574104a9eeb0924ccad
prasad110501 <pshimpi@syr.edu>
2026-09-01 15:15:13 +0000

    new repo and multipage application

---
+++ Lab2.py
@@ -0,0 +1,5 @@
+import streamlit as st 
+
+st.title("Lab 2")
+st.set_page_config(page_title= "Lab 1 HCAI", page_icon=None, layout="wide",
+initial_sidebar_state="auto")
---
+++ streamlit_app.py
@@ -0,0 +1,11 @@
+import streamlit as st 
+
+st.set_page_config(page_title= "Lab assignments HCAI", page_icon=None, layout="wide",
+initial_sidebar_state="auto")
+
+page1 = st.Page('Lab1.py', title = "Lab 1")
+page2 = st.Page('Lab2.py', title = "Lab 2")
+
+pg = st.navigation([page1, page2])
+st.set_page_config(page_title= "Multipage Application - IST688 HCAI")
+pg.run()
